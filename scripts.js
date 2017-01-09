e75241ba1e233fe1ee88f30685b05c7079cca9f1
Nicholas Mead and Nicole Sanders <nicholas@>
2017-01-09 13:47:33 -0800

    add functions to capitalize the first and last letter and to reverse those letters

--- scripts.js
+++ scripts.js
@@ -1,2 +1,14 @@
-var test = prompt("Type a sentence...");
-console.log(test);
+var sentence = prompt("Type a sentence...");
+console.log(sentence);
+
+var firstLastCap = function (s) {
+  return s.charAt(0).toUpperCase() + s.slice(1, s.length - 2) + s.charAt(s.length - 1).toUpperCase();
+};
+
+console.log(firstLastCap(sentence));
+
+var reverse = function (s) {
+  return s.charAt(s.length - 1).toUpperCase() + s.slice(1, s.length - 2) + s.charAt(0).toUpperCase();
+};
+
+console.log(reverse(sentence));
