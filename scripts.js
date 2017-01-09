f924f650a69abc834dcec81a2564af6806a5d1c2
Nicholas Mead and Nicole Sanders <nicholas@>
2017-01-09 14:31:56 -0800

    add function to reverse the sentence with the capitalized first and last letters attached to the end and beginning

--- scripts.js
+++ scripts.js
@@ -1,14 +1,39 @@
 var sentence = prompt("Type a sentence...");
-console.log(sentence);
+//console.log(sentence);
 
 var firstLastCap = function (s) {
-  return s.charAt(0).toUpperCase() + s.slice(1, s.length - 2) + s.charAt(s.length - 1).toUpperCase();
+  return s.charAt(0).toUpperCase() + s.charAt(s.length - 1).toUpperCase();
 };
 
-console.log(firstLastCap(sentence));
+//console.log(firstLastCap(sentence));
 
 var reverse = function (s) {
-  return s.charAt(s.length - 1).toUpperCase() + s.slice(1, s.length - 2) + s.charAt(0).toUpperCase();
+  return s.charAt(s.length - 1).toUpperCase() + s.charAt(0).toUpperCase();
 };
 
-console.log(reverse(sentence));
+var call = function (s) {
+  return(s + reverse(s))
+  //console.log(firstLastCap(s));
+  //console.log(reverse(s));
+};
+
+var reverseStr = function (s) {
+  var arr = s.split("");
+  var newArr = [];
+
+  for (var i = arr.length; i >= 0; i--) {
+    newArr.push(arr[i]);
+  }
+
+  return newArr.join("");
+}
+
+var letterCount = function (s) {
+  var count = Math.floor(s.length/2);
+  //console.log(count);
+  var char = s.charAt(count);
+  //console.log(char);
+  return char + call(s);
+}
+
+console.log(reverseStr(letterCount(sentence)));
